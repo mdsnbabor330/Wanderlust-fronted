67dc0e0c9ac2a2c3ebbce9a9c64fe8f19fba954d
Md Sn Babor <mdsnbabor828@gmail.com>
2026-07-05 21:41:48 +0600

    fix json file

--- src/app/add-destination/page.jsx
+++ src/app/add-destination/page.jsx
@@ -18,14 +18,14 @@ const AddDestination = () => {
         })
 
         const data = await res.json()
-        console.log(data);
+        console.log('after insert',data);
     }
 
 
     return (
         <form onSubmit={onSubmit}
 
-            className="m-20 p-5 space-y-8 border border-gray-200 shadow-sm rounded-2xl"
+            className="m-20 p-5 max-w-350 mx-auto space-y-8 border border-gray-200 shadow-sm rounded-2xl"
           >
             <h1 className='text-3xl font-bold'>Add Destination</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
